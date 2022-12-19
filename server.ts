bce9534b06df9216c0446ff0aadc3d8e99eeb8c4
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2022-12-19 20:16:17 +0300

    first commit

---
+++ server.ts
@@ -0,0 +1,58 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Handlebars = require("handlebars");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT;
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error: string) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+app.set("view engine", "handlebars");
+
+app.engine(
+  "handlebars",
+  engine({
+    extname: "handlebars",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    helpers: {
+      dotdotdot: function (str: string) {
+        if (str.length > 40) return str.substring(0, 40) + "...";
+        return str;
+      },
+    },
+  })
+);
+
+app.set("views", path.join(__dirname, "/views/"));
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
