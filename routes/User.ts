1e03aa3adab71adfc35f3623cbb3a1beb9fa64f6
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2022-12-24 19:48:56 +0300

    User Login

--- routes/User.ts
+++ routes/User.ts
@@ -1,9 +1,53 @@
 import express from "express";
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+const { serialize } = require("cookie");
+require("dotenv").config();
+
+const secret: string = process.env.SECRET;
 
 router.get("/register", async (req: any, res: any) => {
   res.render("user/register");
+});
+
+router.get("/login", async (req: any, res: any) => {
+  res.render("user/login");
+});
+
+router.post("/login", async (req: any, res: any) => {
+  const { username, password } = req.body;
+
+  const getuser = await User.findOne({ username: username });
+
+  try {
+    if (await bcrypt.compare(password, getuser.password)) {
+      const token = sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, // 30 days
+          id: getuser._id,
+        },
+        secret
+      );
+
+      const serialised = serialize("OursiteJWT", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
+      res.setHeader("Set-Cookie", serialised);
+
+      res.status(200).redirect("/");
+    } else {
+      console.log("Kullanıcı Adı yada Şifre Yanlış");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.post("/register", async (req: any, res: any) => {
