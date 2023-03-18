98e37aef68e1904589c45ed73df86e08ac255388
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2023-03-19 00:43:37 +0300

    Menu Filter

---
+++ public/js/menu.js
@@ -0,0 +1,25 @@
+const catnames = document.getElementsByClassName("selectrow");
+const catnames_array = [];
+
+for (i = 0; i < catnames.length; i++) {
+  catnames_array.push(catnames[i].classList[2]);
+}
+
+const filterfunc = (name) => {
+  const itemname = name.innerText;
+
+  const filtered = catnames_array.filter((item) => item === itemname);
+  const otherfiltered = catnames_array.filter(
+    (item) => !item.includes(itemname)
+  );
+
+  for (i = 0; i < otherfiltered.length; i++) {
+    const item = document.getElementsByClassName(otherfiltered[i]);
+    item[0].style.display = "none";
+  }
+
+  for (i = 0; i < filtered.length; i++) {
+    const item = document.getElementsByClassName(filtered[i]);
+    item[0].style.display = "block";
+  }
+};
