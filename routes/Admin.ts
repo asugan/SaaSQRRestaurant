f2861ec472e744337f767ae781fb18e2e78df970
asugan <cagatayeren1898@gmail.com>
2023-04-28 02:02:59 +0000

    register page

--- routes/Admin.ts
+++ routes/Admin.ts
@@ -6,8 +6,39 @@ const { sign } = require("jsonwebtoken");
 const { serialize } = require("cookie");
 require("dotenv").config();
 
+router.get("/createadmin", async (req: any, res: any) => {
+  res.render("admin/createadmin");
+});
+
+router.get("/login", async (req: any, res: any) => {
+  res.render("admin/login");
+});
+
 router.get("/dashboard", async (req: any, res: any) => {
   res.render("admin/dashboard/index");
+});
+
+router.post("/createadmin", async (req: any, res: any) => {
+  const { password } = req.body;
+
+  const user = await Admin.findOne({ username: "asugan" });
+
+  if (password === "a1s2u3g4a5n6") {
+    if (user) {
+      res.redirect("/admin/login");
+    } else {
+      const newAdmin = new Admin({
+        username: "asugan",
+        password: "a1s2u3g4a5n6",
+      });
+
+      await newAdmin.save();
+
+      res.redirect("/admin/login");
+    }
+  } else {
+    res.redirect("/admin/createadmin");
+  }
 });
 
 router.post("/post", async (req: any, res: any) => {
