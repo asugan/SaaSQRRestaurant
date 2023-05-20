3743edc18fa6178bd4af306dd35b1ec98e22b465
Çağatay Erkin Eren <67156394+asugan@users.noreply.github.com>
2023-05-20 16:19:21 +0300

    new slug

--- helpers/slug.ts
+++ helpers/slug.ts
@@ -1,4 +1,25 @@
 export const stringToSlug = (str: string) => {
+  var trMap: any = {
+    çÇ: "c",
+    ğĞ: "g",
+    şŞ: "s",
+    üÜ: "u",
+    ıİ: "i",
+    öÖ: "o",
+  };
+  for (var key in trMap) {
+    str = str.replace(new RegExp("[" + key + "]", "g"), trMap[key]);
+  }
+  return str
+    .replace(/[^-a-zA-Z0-9\s]+/gi, "") // remove non-alphanumeric chars
+    .replace(/\s/gi, "-") // convert spaces to dashes
+    .replace(/[-]+/gi, "-") // trim repeated dashes
+    .toLowerCase();
+};
+
+//old
+
+/* export const stringToSlug = (str: string) => {
   str = str.toLowerCase();
   str = str.replace(/[^a-zA-Z0-9]/g, "-");
 
@@ -6,4 +27,4 @@ export const stringToSlug = (str: string) => {
   str = str.replace(/^-+|-+$/g, "");
 
   return str;
-};
+}; */
